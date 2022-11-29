5cc877580372b9f877b0b339b778f0af0e48bf27
warlord200 <95290000+warlord200@users.noreply.github.com>
2022-11-29 16:31:38 +0000

    Form layout and added submit event

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const page = document.querySelector("#page");
+const remarks = document.querySelector("#remarks");
+const submit = document.querySelector("#submit");
+
 let myLibrary = [];
 
 function Book(title, author, page, remarks) {
@@ -14,3 +20,17 @@ function Book(title, author, page, remarks) {
 function addBookToLibrary(title, author, page, remarks) {
   myLibrary.push(new Book(title, author, page, remarks));
 }
+
+submit.addEventListener(
+  "click",
+  (e) => {
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const page = document.querySelector("#page");
+    const remarks = document.querySelector("#remarks");
+    e.preventDefault();
+    addBookToLibrary(title.value, author.value, page.value, remarks.value);
+    console.log(myLibrary);
+  },
+  false
+);
