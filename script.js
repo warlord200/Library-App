8ac9d105151e0039d709a9c08ee54f6c1705802d
warlord200 <95290000+warlord200@users.noreply.github.com>
2022-11-30 16:14:21 +0000

    Main layout completed and cards script completed

--- script.js
+++ script.js
@@ -6,19 +6,46 @@ const submit = document.querySelector("#submit");
 
 let myLibrary = [];
 
-function Book(title, author, page, remarks) {
+function book(title, author, page, remarks) {
   this.title = title;
   this.author = author;
   this.page = page;
   this.remarks = remarks;
-  this.info = function () {
-    console.log(`${title} by ${author}, ${page} pages, ${remarks}`);
-  };
   // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
 }
+book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.page} pages, ${this.remarks}`;
+};
 
 function addBookToLibrary(title, author, page, remarks) {
-  myLibrary.push(new Book(title, author, page, remarks));
+  myLibrary.push(new book(title, author, page, remarks));
+}
+
+function updateLibrary() {
+  const bookList = document.querySelector("#book-list");
+  const card = document.createElement("div");
+  card.classList.add("card");
+  bookList.appendChild(card);
+  const cardTitle = document.createElement("div");
+  cardTitle.classList.add("card-title");
+  const cardAuthor = document.createElement("div");
+  cardAuthor.classList.add("card-author");
+  const cardPage = document.createElement("div");
+  cardPage.classList.add("card-page");
+  const cardRemarks = document.createElement("div");
+  cardRemarks.classList.add("card-remarks");
+  const cardDelete = document.createElement("div");
+  cardDelete.classList.add("card-delete");
+  cardDelete.innerHTML = "Delete";
+  cardTitle.textContent = title.value;
+  cardAuthor.textContent = author.value;
+  cardPage.textContent = page.value;
+  cardRemarks.textContent = remarks.value;
+  card.appendChild(cardTitle);
+  card.appendChild(cardAuthor);
+  card.appendChild(cardPage);
+  card.appendChild(cardRemarks);
+  card.appendChild(cardDelete);
 }
 
 submit.addEventListener(
@@ -29,8 +56,16 @@ submit.addEventListener(
     const page = document.querySelector("#page");
     const remarks = document.querySelector("#remarks");
     e.preventDefault();
+    if (
+      title.value === "" ||
+      author.value === "" ||
+      page.value === "" ||
+      remarks.value === ""
+    )
+      return;
     addBookToLibrary(title.value, author.value, page.value, remarks.value);
     console.log(myLibrary);
+    updateLibrary();
   },
   false
 );
